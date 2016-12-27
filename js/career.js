e42f10e551f6da8830bf34065f13d0360ae2babb
asrinivas <asrinivas@paypal.com>
2016-12-27 14:52:46 -0800

    Formatting Changes

--- js/career.js
+++ js/career.js
@@ -1,38 +1,22 @@
+$("#live-search").submit(function() {
 
-$("#live-search").submit(function(){
-
-  var res=$("#search-box").val();
-var searchQuery = res.toLowerCase();
-if( "web application development (ui/ux) ".indexOf(searchQuery) > -1 ) {
-window.location = "#web-application";
-}
-else if("devops".indexOf(searchQuery) > -1 )
-    {
+    var res = $("#search-box").val();
+    var searchQuery = res.toLowerCase();
+    if ("web application development (ui/ux) ".indexOf(searchQuery) > -1) {
+        window.location = "#web-application";
+    } else if ("devops".indexOf(searchQuery) > -1) {
         window.location = "#support-operation";
-    }
-else if("testing as services".indexOf(searchQuery) > -1 )
-    {
+    } else if ("testing as services".indexOf(searchQuery) > -1) {
         window.location = "#testing-services";
-    }
-else if("mobility".indexOf(searchQuery) > -1 )
-    {
+    } else if ("mobility".indexOf(searchQuery) > -1) {
         window.location = "#mobility";
-    }
-else if("big data / analytics".indexOf(searchQuery) > -1 )
-    {
+    } else if ("big data / analytics".indexOf(searchQuery) > -1) {
         window.location = "#data-analytics";
-    }
-else if("application / software development".indexOf(searchQuery) > -1 )
-    {
+    } else if ("application / software development".indexOf(searchQuery) > -1) {
         window.location = "#app-software-development";
-    }
-else if("consulting services".indexOf(searchQuery) > -1 )
-    {
+    } else if ("consulting services".indexOf(searchQuery) > -1) {
         window.location = "#consulting-services";
-    }
-
-    else
-    {
+    } else {
         document.getElementById("search").submit();
     }
 
